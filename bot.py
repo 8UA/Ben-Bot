07bd56061145426a8bad5e0b7f03952dbb26edcc
eremophobia <succducc0@gmail.com>
2022-04-02 07:43:52 +0200

    - Ben Bot

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from random import choice
+
+client = discord.Client()
+ben_answers = [ "Yes." , "No." , "Hohoho" , "Eugh..." ]
+
+@client.event
+async def on_ready():
+    print('Ben is online!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('ben') or message.content.startswith('Ben'):
+        await message.channel.send(choice(ben_answers))
+
+client.run('BOT_TOKEN')
